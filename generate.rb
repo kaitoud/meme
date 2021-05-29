4379b7bce37e3f1b3b6384f574c0ec16da0e9b57
kaitoud <thaidn59@gmail.com>
2021-05-29 18:06:31 +0700

    commit-0

--- generate.rb
+++ generate.rb
@@ -4,7 +4,7 @@ puts 'aaaa'
 FILE_NAME = 'log.txt'
 
 
-[1..10].each do |no|
+10.times.each do |no|
   system("echo #{no} >> #{FILE_NAME}")
   system("git add .")
   system("git commit -m commit-#{no}")
